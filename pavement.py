d24ebaefcf5d144e24354d7e424a2a6620568b63
Amit Shah <amitshah@oneuk.com>
2014-08-26 17:52:27 +0100

    Initial commit

---
+++ pavement.py
@@ -0,0 +1,31 @@
+from paver.easy import *
+from paver.setuputils import setup
+
+setup(
+    name="Jenkins-Alfred-Workflow",
+    packages=['jenkins_workflow'],
+    version="1.0",
+    url="http://www.amwam.me/",
+    author="Amit Shah",
+    author_email="amitshah@oneuk.com"
+)
+
+
+@task
+@needs(["distutils.command.sdist"])
+def sdist():
+    """Generate docs and source distribution."""
+    pass
+
+
+@task
+@needs('unit_tests')
+def default():
+    """Default tasks to execute when running paver from the command line."""
+
+
+@task
+def unit_tests():
+    """Runs all unit tests under the test/unit folder structure."""
+    sh("nosetests test/unit --with-cov --cov-report term-missing")
+
