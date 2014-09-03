92a38a76b8e614bbd6420aa65c97d140828ec91a
Amit Shah <amitshah@oneuk.com>
2014-09-03 16:57:37 +0100

    Added functionality to perform lookups using the jenkins api

---
+++ main.py
@@ -0,0 +1,26 @@
+from workflow import Workflow
+from workflow.jenkins.jenkins_interface import JenkinsInterface
+
+
+def main(wf):
+    command = wf.args[0]
+    query = wf.args[1] if len(wf.args) > 1 else None
+    interface = JenkinsInterface(wf)
+
+    options = {
+        'set_url': interface.set_jenkins_url,
+        'failing': interface.get_failed_jobs,
+        'building': interface.get_building_jobs,
+        'all': interface.get_all_jobs
+    }
+
+    for job in options[command](query):
+        wf.add_item(job.name, arg=job.url, valid=True, icon=job.image)
+
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    print Workflow().run(main)
+
+
